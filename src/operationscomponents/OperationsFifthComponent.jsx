06bf5dd8848dc1bc569b7c13b8d39137910fd273
sidd84755 <sidd84755@gmail.com>
2023-06-04 16:30:53 +0530

    Implemented U.I changes

--- src/operationscomponents/OperationsFifthComponent.jsx
+++ src/operationscomponents/OperationsFifthComponent.jsx
@@ -12,37 +12,37 @@ const OperationsFifthSection = () => {
             <Typography paddingX={{xs:'25px', sm:'30px',md:'50px'}} fontSize={{xs:'20px',sm:'22px',md:'72px',lg:'80px'}} fontWeight={700} textAlign='center' color='#A17337' lineHeight='94px'>
                 QUALITY
             </Typography>
-            <Box height='10rem'>
+            <Box height='7rem'>
 
             </Box>
         </Box>
         <Box bgcolor='#A17337' paddingY={{xs:'25px', sm:'30px',md:'150px'}} display='flex' flexDirection='column' gap='1rem'>
-            <Typography paddingX={{xs:'25px', sm:'30px',md:'50px'}} fontSize={{xs:'20px',sm:'22px',md:'18px'}} fontWeight={400} color='#E5E5E5' lineHeight='28px'>
-                AP Minerals is one of the very few coal companies that are quality conscious and thus,<br/>deliver only quality coal complying to
-                international standards. Our excellent process and<br/>commitment to
+            <Typography paddingX={{xs:'25px', sm:'30px',md:'50px'}} fontSize={{xs:'12px',sm:'12px',md:'14px',lg:'14px'}} fontWeight={400} color='#E5E5E5' lineHeight='20px'>
+                AP Minerals is one of the very few coal companies that are quality conscious and thus, deliver only quality coal complying to
+                international standards. Our excellent process and commitment to
                 meet our clients' expectations by rendering finest products is
-                responsible<br/>for our unparalleled growth in the last few years.
+                responsible for our unparalleled growth in the last few years.
             </Typography>
-            <Typography paddingX={{xs:'25px', sm:'30px',md:'50px'}} fontSize={{xs:'20px',sm:'22px',md:'18px'}} fontWeight={400} color='#E5E5E5' lineHeight='28px'>
+            <Typography paddingX={{xs:'25px', sm:'30px',md:'50px'}} fontSize={{xs:'12px',sm:'12px',md:'14px',lg:'14px'}} fontWeight={400} color='#E5E5E5' lineHeight='20px'>
                 Our professionals adhere to strict quality
                 standards and carry out careful testing of
-                the<br/>products on various parameters prior to
+                the products on various parameters prior to
                 the final dispatching process. The sampling
-                and<br/>analysis we appoint are well qualified
-                representatives of inspection agencies as per<br/>international standards at stockpile,
+                and analysis we appoint are well qualified
+                representatives of inspection agencies as per international standards at stockpile,
                 jetty and load ports. This way it
-                accommodates us in<br/>maintaining the quality
+                accommodates us in maintaining the quality
                 of the coal to meet and as per the desired
-                needs and specifications<br/>of our clients.
+                needs and specifications of our clients.
                 Since our client satisfaction is our success
-                mantra; we leave no stone<br/>unturned in
+                mantra; we leave no stone unturned in
                 ensuring that all the goods supplied by us
                 fall under the highest quality range.
             </Typography>
-            <Typography paddingX={{xs:'25px', sm:'30px',md:'50px'}} fontSize={{xs:'20px',sm:'22px',md:'18px'}} fontWeight={400} color='#E5E5E5' lineHeight='28px'>
+            <Typography paddingX={{xs:'25px', sm:'30px',md:'50px'}} fontSize={{xs:'12px',sm:'12px',md:'14px',lg:'14px'}} fontWeight={400} color='#E5E5E5' lineHeight='20px'>
                 We mine and process the products under strict examination and scrutiny
-                by our team of<br/>experts. Our quality coal products have been highly
-                appreciated by the clients for its<br/>minimal ash content, great carbon
+                by our team of experts. Our quality coal products have been highly
+                appreciated by the clients for its minimal ash content, great carbon
                 content and user specified calorific value.
             </Typography>
         </Box>  
--- src/operationscomponents/OperationsSixthComponent.jsx
+++ src/operationscomponents/OperationsSixthComponent.jsx
@@ -8,7 +8,7 @@ const OperationsSixthSection = () => {
       <Box
         display='flex' flexDirection='row' justifyContent='center'
       >
-        <Typography paddingY={{xs:'25px', sm:'30px',md:'80px'}} paddingX={{xs:'25px', sm:'30px',md:'150px'}} fontSize={{xs:'20px',sm:'22px',md:'28px'}} fontWeight={500} textAlign='center' color='#fff' lineHeight='35px'>
+        <Typography paddingY={{xs:'25px', sm:'30px',md:'80px'}} paddingX={{xs:'25px', sm:'30px',md:'150px'}} fontSize={{xs:'20px',sm:'22px',md:'22px',lg:'24px'}} fontWeight={500} textAlign='center' color='#fff' lineHeight='34px'>
             “Our foremost goal is to cater the requirements of our nationwide<br/>
             as well as global customers by rendering high quality coal and<br/>
             ensure utmost client satisfaction.”
