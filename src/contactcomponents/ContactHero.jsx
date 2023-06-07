88f740653cccac6443f47ed53fd184ac363ed891
sidd84755 <sidd84755@gmail.com>
2023-06-07 18:14:09 +0530

    Worked on responsiveness

--- src/contactcomponents/ContactHero.jsx
+++ src/contactcomponents/ContactHero.jsx
@@ -5,10 +5,10 @@ import { contactmainBg } from '../assets'
 const ContactHero = () => {
   return (
     <>
-      <Box sx={{height:'76vh', backgroundImage:`url(${contactmainBg})`,backgroundRepeat:'no-repeat',backgroundSize:'100%',zIndex:1}}
+      <Box sx={{height:'76vh', backgroundImage:`url(${contactmainBg})`,backgroundRepeat:'no-repeat',backgroundSize:'100% 100%',zIndex:1}}
         display='flex' flexDirection='row' justifyContent='flex-start' alignItems='flex-end'
       >
-        <Typography paddingY={{xs:'25px', sm:'30px',md:'80px'}} paddingX={{xs:'25px', sm:'30px',md:'150px'}} fontSize={{xs:'20px',sm:'22px',md:'72px',lg:'86px'}} fontWeight={700} textAlign='center' color='#E5E5E5' lineHeight='100px'>
+        <Typography paddingY={{xs:'25px', sm:'30px',md:'80px'}} paddingX={{xs:'25px', sm:'30px',md:'150px'}} fontSize={{xs:'52px',sm:'52px',md:'72px',lg:'86px'}} fontWeight={700} textAlign='center' color='#E5E5E5' lineHeight={{xs:'61px',sm:'61px',md:'100px'}}>
             Contact Us
         </Typography>    
       </Box>
--- src/contactcomponents/ContactSecondComponent.jsx
+++ src/contactcomponents/ContactSecondComponent.jsx
@@ -6,8 +6,8 @@ import { mainsectionImg } from '../assets'
 const ContactSecondSection = () => {
   return (
     <>
-      <Box paddingY={{xs:'25px', sm:'30px',md:'50px'}} display='flex' flexDirection={{xs:'column',sm:'column',md:'row'}} justifyContent='space-around'
-        sx={{background:'rgba(0, 0, 0, 0.8)'}}
+      <Box paddingY={{xs:'25px', sm:'30px',md:'50px'}} paddingX={{xs:'40px', sm:'40px',md:'50px'}} display='flex' flexDirection={{xs:'column',sm:'column',md:'row'}} justifyContent={{xs:'center',sm:'center',md:'space-around'}} alignItems='center'
+        sx={{background:'rgba(0, 0, 0, 0.8)'}} gap={{xs:'2rem',sm:'2rem',md:'0'}}
         
       >
         <Box display='flex' flexDirection='column' gap='2rem'>
@@ -15,38 +15,38 @@ const ContactSecondSection = () => {
                 <img src={mainsectionImg} alt="" />
             </Box>
             <Box display='flex' flexDirection='column' gap='1rem'>
-                <Typography fontSize={{xs:'16px',sm:'18px',md:'20px',lg:'20px'}} fontWeight={600} color='#CDA571' lineHeight='28px'>
+                <Typography fontSize={{xs:'24px',sm:'24px',md:'20px',lg:'20px'}} fontWeight={600} color='#CDA571' lineHeight='28px'>
                     Office
                 </Typography>
-                <Typography fontSize={{xs:'12px',sm:'12px',md:'14px',lg:'14px'}} fontWeight={400} color='#CDA571' lineHeight='20px'>
+                <Typography fontSize={{xs:'18px',sm:'18px',md:'14px',lg:'14px'}} fontWeight={400} color='#CDA571' lineHeight='28px'>
                     8 Avenue Business Centre,<br/>
                     A-3-9, Jalan 8/1, 46050 Petaling Jaya,<br/>
                     Selangor, Malaysia.
                 </Typography>
             </Box>
             <Box display='flex' flexDirection='column' gap='1rem'>
-                <Typography fontSize={{xs:'16px',sm:'18px',md:'20px',lg:'20px'}} fontWeight={600} color='#CDA571' lineHeight='28px'>
+                <Typography fontSize={{xs:'24px',sm:'24px',md:'20px',lg:'20px'}} fontWeight={600} color='#CDA571' lineHeight='28px'>
                     Phone
                 </Typography>
-                <Typography fontSize={{xs:'12px',sm:'12px',md:'14px',lg:'14px'}} fontWeight={400} color='#CDA571' lineHeight='20px'>
+                <Typography fontSize={{xs:'18px',sm:'18px',md:'14px',lg:'14px'}} fontWeight={400} color='#CDA571' lineHeight='28px'>
                     +60 17 620 2105 &nbsp; +91 98931 15454
                 </Typography>
             </Box>
             <Box display='flex' flexDirection='column' gap='1rem'>
-                <Typography fontSize={{xs:'16px',sm:'18px',md:'20px',lg:'20px'}} fontWeight={600} color='#CDA571' lineHeight='28px'>
+                <Typography fontSize={{xs:'24px',sm:'24px',md:'20px',lg:'20px'}} fontWeight={600} color='#CDA571' lineHeight='28px'>
                     E-mail
                 </Typography>
-                <Typography fontSize={{xs:'12px',sm:'12px',md:'14px',lg:'14px'}} fontWeight={400} color='#CDA571' lineHeight='20px'>
+                <Typography fontSize={{xs:'18px',sm:'18px',md:'14px',lg:'14px'}} fontWeight={400} color='#CDA571' lineHeight='28px'>
                     info@apmineral.in 
                 </Typography>
             </Box>
         </Box>
         <Box display='flex' flexDirection='column' alignItems='flex-start' justifyContent='center' gap='0.5rem'>
-            <Typography fontSize={{xs:'12px',sm:'16px',md:'18px',lg:'20px'}} fontWeight={400} color='#A17337' fontStyle='italic'>
+            <Typography fontSize={{xs:'24px',sm:'24px',md:'24px',lg:'20px'}} fontWeight={400} color='#A17337' fontStyle='italic'>
                 Contact Us
             </Typography>
             <Box display='flex' flexDirection='column' alignItems='flex-start' justifyContent='center' gap='1.5rem'>
-                <Typography fontSize={{xs:'16px',sm:'22px',md:'32px',lg:'36px'}} fontWeight={700} color='#fff' lineHeight='42px'>
+                <Typography fontSize={{xs:'30px',sm:'30px',md:'32px',lg:'36px'}} fontWeight={700} color='#fff' lineHeight={{xs:'35px',sm:'35px',md:'42px'}}>
                     We’re Looking Forward to<br/>Hearing From You
                 </Typography>
                 <ContactForm/>
